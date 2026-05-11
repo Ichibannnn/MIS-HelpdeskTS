d64faa66b2a2b4e56bdf2e04e7b183286fd3202e
Ichibannnn <guser9897@gmail.com>
2026-05-11 16:51:34 +0800

    Update: Initial sidebar and header

--- src/layout/Landing.tsx
+++ src/layout/Landing.tsx
@@ -1,37 +1,50 @@
-import { useNavigate } from "react-router";
-import { useAppDispatch, useAppSelector } from "../hooks/useRedux";
-import { Button, IconButton } from "@mui/material";
-import { signOut } from "../features/auth/authSlice";
-import { clearUserDetails } from "../features/user/userSlice";
-import { useThemeMode } from "../context/ThemeContext";
-import { DarkMode, LightMode } from "@mui/icons-material";
+// import { useNavigate } from "react-router";
+// import { useAppDispatch, useAppSelector } from "../hooks/useRedux";
+// import { Button, IconButton } from "@mui/material";
+// import { signOut } from "../features/auth/authSlice";
+// import { clearUserDetails } from "../features/user/userSlice";
+// import { useThemeMode } from "../context/ThemeContext";
+// import { DarkMode, LightMode } from "@mui/icons-material";
+
+import { Box, Stack } from "@mui/material";
+import { Outlet } from "react-router";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 const Landing = () => {
-  const userDetails = useAppSelector((state) => state);
-  const { mode, toggleTheme } = useThemeMode();
+  // const userDetails = useAppSelector((state) => state);
+  // const { mode, toggleTheme } = useThemeMode();
 
-  const navigate = useNavigate();
-  const dispatch = useAppDispatch();
+  // const navigate = useNavigate();
+  // const dispatch = useAppDispatch();
 
-  console.log("UserDetails: ", userDetails);
+  // console.log("UserDetails: ", userDetails);
 
-  const onLogoutHandler = () => {
-    dispatch(signOut());
-    dispatch(clearUserDetails());
+  // const onLogoutHandler = () => {
+  //   dispatch(signOut());
+  //   dispatch(clearUserDetails());
 
-    navigate("/login");
-  };
+  //   navigate("/login");
+  // };
 
   return (
-    <>
-      <Button variant="contained" onClick={onLogoutHandler}>
-        Logout
-      </Button>
+    // <>
+    //   <Button variant="contained" onClick={onLogoutHandler}>
+    //     Logout
+    //   </Button>
 
-      <IconButton color="inherit" onClick={toggleTheme}>
-        {mode === "light" ? <DarkMode /> : <LightMode />}
-      </IconButton>
-    </>
+    //   <IconButton color="inherit" onClick={toggleTheme}>
+    //     {mode === "light" ? <DarkMode /> : <LightMode />}
+    //   </IconButton>
+    // </>
+
+    <Stack sx={{ flexDirection: "row", height: "100vh" }}>
+      <Sidebar />
+      <Box>
+        <Header />
+        <Outlet />
+      </Box>
+    </Stack>
   );
 };
 
